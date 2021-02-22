ed40eaef9e936abfcf1a63b57b8a7bc1c0d47b96
Yitik Kawatra <yitikkawatra@gmail.com>
2021-02-22 15:36:14 +0530

    notes app added

---
+++ app.js
@@ -0,0 +1,87 @@
+shownotes()
+let addBtn=document.getElementById('addBtn');
+
+addBtn.addEventListener('click',function(e){
+
+    let addTxt=document.getElementById('addTxt');
+    let addTitle=document.getElementById('addTitle');
+    let notes=localStorage.getItem("notes");
+
+    if(notes==null){
+        notesObj=[];
+    }
+    else{
+        notesObj=JSON.parse(notes);
+    }
+    let myObj={
+        title:addTitle.value,
+        text:addTxt.value
+    }
+    notesObj.push(myObj);
+    localStorage.setItem("notes",JSON.stringify(notesObj));
+    addTxt.value="";
+    addTitle.value="";
+  
+    shownotes();
+})
+function shownotes(){
+    let notes=localStorage.getItem("notes");
+    if(notes==null){
+        notesObj=[];
+    }
+    else{
+        notesObj=JSON.parse(notes);   
+    }
+    let html="";
+        notesObj.forEach(function(element,index){
+            html+=` <div class="noteCard m-2 card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Note ${element.title}</h5>
+              <p class="card-text">${element.text} </p>
+              <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Node</button>
+            </div>
+         </div>
+           `;   
+           
+        });
+        let notesElm=document.getElementById('notes');
+        if(notesObj.length!=0){
+            notesElm.innerHTML=html;
+        }
+        else{
+                notesElm.innerHTML=`Nothing to show! Use "Add a note" section above to add a note.`;
+                console.log()
+        }
+}
+
+function deleteNote(index){
+    let notes=localStorage.getItem("notes");
+    if(notes==null){
+        notesObj=[];
+    }
+    else{
+        notesObj=JSON.parse(notes);   
+    }
+        notesObj.splice(index,1);
+        localStorage.setItem("notes",JSON.stringify(notesObj));
+        shownotes();    
+}
+
+let search=document.getElementById('searchTxt');
+search.addEventListener('input',function(){
+let inputVal=search.value.toLowerCase();
+let noteCards=document.getElementsByClassName('noteCard');
+
+Array.from(noteCards).forEach(function(element){
+        let cardTxt=element.getElementsByTagName('p')[0].innerText.toLowerCase;
+        console.log
+        if(cardTxt.includes(inputVal)){
+            element.style.display="block";
+
+        }
+        else{
+            element.style.display="none";
+        }
+
+       })
+})
